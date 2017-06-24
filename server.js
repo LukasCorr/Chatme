f4073d1bde245a71145fa241a9fb03b3b6c1230d
Lukas <correa97lukas@hotmail.com>
2017-06-24 18:04:21 -0300

    new design

--- server.js
+++ server.js
@@ -9,9 +9,20 @@ server.listen(8888,function(){
 
 app.use(express.static(__dirname + '/'));
 
+var visitas = 0;
 io.sockets.on('connection', function(socket){
+	
+	visitas++;
+	socket.emit('vtas', visitas);
+	socket.broadcast.emit('vtas', visitas);
+
 	socket.on('send message', function(data){
 	io.sockets.emit('new message', data);
 	});
-});
 
+	socket.on('disconnect', function(){
+     	visitas--;
+     	socket.broadcast.emit('vtas', visitas);
+ 	});
+});
+	
